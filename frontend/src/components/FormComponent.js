a393fb3a082c9213b3ab7d117160ad1b71d2762e
ibm-macbook <mpronesti@massimilianos-MacBook-Pro.local>
2024-09-11 13:44:14 +0100

    initial point

--- frontend/src/components/FormComponent.js
+++ frontend/src/components/FormComponent.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Form, Button, Container, Row, Col, Alert } from 'react-bootstrap';
 
+const WiRDescr = `The Women in Red WikiProject focuses on creating content regarding women's biographies,
+women's works, and women's issues. The objective is to turn "redlinks" on Wikipedia into blue ones, and by doing so,
+reduce Wikipedia's systematic gaps in its coverage of under-represented groups`;
+
 function FormComponent(props) {
   const base_url = process.env.REACT_APP_BACKEND_BASE_ENDPOINT;
 
   // Unpack props
-  const { onQuery,
-    wikiPage, setWikiPage,
-    formData, setFormData,
-    showAlert, setShowAlert,
-    alertMessage, setAlertMessage
-  } = props;
+  const { onQuery, wikiPage, setWikiPage, formData, setFormData, showAlert, setShowAlert, alertMessage, setAlertMessage } = props;
+
   // States
-  const [categories, setCategories] = useState([]);
   const [dataSources, setDataSources] = useState([]);
   const [languages, setLanguages] = useState({});
   const [models, setModels] = useState({});
@@ -22,16 +21,15 @@ function FormComponent(props) {
     async function fetchFormData() {
       try {
         const response = await axios.get(`${base_url}/form-data`);
-        setCategories(response.data.categories);
         setDataSources(response.data.data_sources);
         setLanguages(response.data.languages);
         setModels(response.data.models);
         setFormData({
-          category: response.data.categories[0],
           dataSource: response.data.data_sources[0],
           language: Object.keys(response.data.languages)[0],
           model: [Object.keys(response.data.models)[0]], // Set initial model key as an array
-          subject: ''
+          subject: '',
+          gender: 'N\\A' // Set default gender
         });
       } catch (error) {
         console.error('Error fetching form data:', error);
@@ -67,18 +65,18 @@ function FormComponent(props) {
     setShowAlert(false);
     if (formData.subject.trim()) {
       // Reset wikipage on a new search
-      if(wikiPage.title !== ''){
-        setWikiPage({ title: '', content: '' })
+      if (wikiPage.title !== '') {
+        setWikiPage({ title: '', content: '' });
       }
       onQuery({
         entity_name: formData.subject,
-        category: formData.category,
         language: formData.language,
         data_source: formData.dataSource,
         model: formData.model, // Send model keys to backend as an array
+        gender: formData.gender // Include gender in the query
       });
     } else {
-      setAlertMessage('Subject cannot be empty');
+      setAlertMessage('Entity cannot be empty');
       setShowAlert(true);
     }
   };
@@ -87,18 +85,65 @@ function FormComponent(props) {
     <Container>
       <Row>
         <Col>
-          <h2>Generate Wikipedia-like Page</h2>
+          <h2>Generate seed Wikipedia page</h2>
           {showAlert && <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>{alertMessage}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="subjectInput" className="mb-3">
-              <Form.Label>Entity:</Form.Label>
+            <div style={{ border: '1.5px solid', padding: '10px', marginBottom: '10px' }}>
+              <Form.Group controlId="subjectInput" className="mb-3">
+                <Form.Label>Entity:</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  placeholder="Type in an entity (person, place, organization, artistic or intellectual work etc.)"
+                />
+              </Form.Group>
+              {/* ----- WiR box ------*/}
+              <div className="woman-in-red-box">
+                <p style={{ color: 'white' }}>Or select a Woman in Red (filter by occupation):</p>
+                <Row>
+                    <Col md={6}>
+                      <Form.Group controlId="occupationInput" className="mb-3">
+                        <Form.Control
+                            as="select"
+                            name="occupation"
+                            value={formData.occupation}
+                            onChange={handleChange}
+                        >
+                          <option>-- Occupation --</option>
+                        </Form.Control>
+                      </Form.Group>
+                      <Form.Group controlId="womanInRedSelect" className="mb-3">
+                        <Form.Control
+                          as="select"
+                          name="womanInRed"
+                          value={formData.womanInRed}
+                          onChange={handleChange}
+                        >
+                          <option>-- Woman in Red --</option>
+                        </Form.Control>
+                      </Form.Group>
+                    </Col>
+                    <Col md={6} style={{ textAlign: 'left', fontSize: '0.8em', color: 'white' }}>
+                      <p>{WiRDescr}</p>
+                    </Col>
+                  </Row>
+                </div>
+            </div>
+            <Form.Group controlId="genderSelect" className="mb-3">
+              <Form.Label>Select Grammatical Gender:</Form.Label>
               <Form.Control
-                type="text"
-                name="subject"
-                value={formData.subject}
-                onChange={handleChange}
-                placeholder="Enter one entity"
-              />
+                  as="select"
+                  name="gender"
+                  value={formData.gender}
+                  onChange={handleChange}
+              >
+                <option value="N\\A">N/A</option>
+                <option value="Male">Masculine</option>
+                <option value="Female">Feminine</option>
+                <option value="Neutral">Neutral</option>
+              </Form.Control>
             </Form.Group>
             <Form.Group controlId="languageSelect" className="mb-3">
               <Form.Label>Select Language:</Form.Label>
@@ -131,21 +176,6 @@ function FormComponent(props) {
                 ))}
               </Form.Control>
             </Form.Group>
-            <Form.Group controlId="categorySelect" className="mb-3">
-              <Form.Label>Select Category:</Form.Label>
-              <Form.Control
-                as="select"
-                name="category"
-                value={formData.category}
-                onChange={handleChange}
-              >
-                {categories.map((category) => (
-                  <option key={category} value={category}>
-                    {category}
-                  </option>
-                ))}
-              </Form.Control>
-            </Form.Group>
             <Form.Group controlId="dataSourceSelect" className="mb-3">
               <Form.Label>Select Data Source:</Form.Label>
               <Form.Control
@@ -162,7 +192,7 @@ function FormComponent(props) {
               </Form.Control>
             </Form.Group>
             <Button variant="primary" type="submit">
-              Query DBpedia
+              Query Data Source
             </Button>
           </Form>
         </Col>
